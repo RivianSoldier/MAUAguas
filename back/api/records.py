9d7f1136ba55f8b1b64c216e73c2ea93c862b4a9
fernandokusko <fernando.freitas@bcrcx.com>
2024-04-30 14:58:39 -0300

    Adicionando rotas de acesso ao banco gets,updates e post, e JSON para aguardar infos dos reservatórios

--- back/api/records.py
+++ back/api/records.py
@@ -1,11 +1,55 @@
 from back.api.database import DataBase
 from back.api.models import ReservoirParameters, ReservoirStatus
+from datetime import datetime
 
 class RecordsDB:
     @staticmethod
     def post_reservoir_parameters(params: ReservoirParameters):
-        DataBase.post_reservoir_parameters(params)
+        id_exists = DataBase.id_exists(params.id)
+        if id_exists:
+            print("Erro: ID already exists")
+        else:
+            DataBase.post_reservoir_parameters(params)
         
     @staticmethod
     def post_reservoir_status(params: ReservoirStatus):
         DataBase.post_reservoir_status(params)
+        
+    @staticmethod
+    def update_height(height:float, id:str):
+        DataBase.update_height(height,id)
+        
+    @staticmethod
+    def update_name(name:str, id:str):
+        DataBase.update_name(name,id)
+        
+    @staticmethod
+    def update_records_limit_1(limit:float, id:str):
+        DataBase.update_alert_limit_1(limit, id)
+    
+    @staticmethod
+    def update_records_limit_2(limit:float, id:str):
+        DataBase.update_alert_limit_2(limit, id)
+    
+    @staticmethod
+    def update_records_limit_3(limit:float, id:str):
+        DataBase.update_alert_limit_3(limit, id)
+        
+    @staticmethod
+    def get_reservoir_by_id(id:str):
+        reservoir = DataBase.get_reservoir_by_id(id)
+        return reservoir
+    
+    @staticmethod
+    def get_reservoir_status_by_date(id:str, start_date: datetime, end_date:datetime):
+        status = DataBase.get_reservoir_status_by_date(id,start_date,end_date)
+        return status
+    
+    @staticmethod
+    def get_reservoir_status_by_id(id:str):
+        status = DataBase.get_reservoir_status_by_id(id)
+        return status
+    
+    def get_lastest_reservoir_status_by_id(id:str):
+        last_status = DataBase.get_lastest_reservoir_status_by_id(id)
+        return last_status
--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 from back.api.database import DataBase
+from back.api.records import RecordsDB
 from back.api.models import ReservoirParameters, ReservoirStatus
 from datetime import datetime
 # Criar um objeto ReservoirParameters com os valores desejados
 params = ReservoirParameters(
     id="teste",
     height=10.5,
+    name="teste_water_tank",
+    well=False,
+    alert_limit_1=20.0,
+    alert_limit_2=30.0,
+    alert_limit_3=40.0
+)
+params_2 = ReservoirParameters(
+    id="teste2",
+    height=10.5,
+    name="teste_water_tank",
+    well=False,
     alert_limit_1=20.0,
     alert_limit_2=30.0,
     alert_limit_3=40.0
@@ -14,12 +26,22 @@ status = ReservoirStatus(
     water_flow_in=100.0,
     water_flow_out=50.0,
     water_humidity=70.0,
-    water_height=8.0,
     water_voltage=12.0,
     time_stamp=datetime.now(),
     bomb_hours=10.0
 )
 
 # Chamar o método post_reservoir_parameters e passar o objeto params como argumento
-#DataBase.post_reservoir_parameters(params)
-#DataBase.post_reservoir_status(status)
+# RecordsDB.post_reservoir_parameters(params)
+# RecordsDB.post_reservoir_parameters(params_2)
+# RecordsDB.post_reservoir_status(status)
+
+RecordsDB.update_height(7,"teste")
+RecordsDB.update_records_limit_1(60,"teste")
+RecordsDB.update_records_limit_2(70,"teste")
+RecordsDB.update_records_limit_3(80,"teste")
+RecordsDB.update_name("Caixa do Daniel","teste")
+print(RecordsDB.get_reservoir_by_id("teste"))
+
+print(RecordsDB.get_reservoir_status_by_id("teste"))
+print(RecordsDB.get_lastest_reservoir_status_by_id("teste"))
