7b233a4aaafc6530bed31abe82b5ac8e23efac33
Eduardo Santiago <105879506+Boujingou@users.noreply.github.com>
2024-05-08 19:14:00 -0300

    Revert "Teste2.0"

--- front/mauaguas_front/app/controle/page.js
+++ front/mauaguas_front/app/controle/page.js
@@ -16,32 +16,32 @@ import { Search } from "lucide-react";
 
 export default function DashboardDetalhado() {
   return (
-    <main className="flex min-h-screen w-full flex-col bg-[#303030] min-w-[320px]">
+    <main className="flex min-h-screen w-full flex-col bg-[#303030]">
       <NavBar activePage="controle" />
-      <div className="w-full mt-32 h-full">
-        <div className='flex justify-start text-2xl ml-10'>
-          <p className='text-white font-bold'>
+      <div className="mt-32">
+        <div className="flex justify-start text-2xl mb-10 ml-10">
+          <p className="text-white font-bold text-2xl">
             Controle do Administrador
           </p>
         </div>
-        <div className='grid grid-cols-4 gap-6'>
+        <div className="flex">
           <LimtCard title="Limite 1 - menos grave" color="#89CAF9"></LimtCard>
           <LimtCard title="Limite 2 - grave" color="#DAE466"></LimtCard>
           <LimtCard title="Limite 3 - mais grave" color="#F989B2"></LimtCard>
         </div>
-        <div className="flex grid grid-cols-6 justify-between mx-10 items-center">
-          <div className="flex lg:col-span-2 md:col-span-3 col-span-4 col-start-2 gap-4 w-full mt-10">
-            <div className="relative w-full">
+        <div className="flex flex-row justify-between mx-10 mt-10">
+          <div className="flex gap-4">
+            <div className="relative">
               <Input
                 placeholder="Procurar..."
-                className="bg-[#505050] border-none text-white h-12 w-full"
+                className="bg-[#505050] border-none text-white w-72 h-12"
               />
               <Search className="absolute right-2 top-3.5 h-5 w-5 text-white" />
             </div>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button
-                  className="min-w-[60px] py-6 bg-[#3B3B3B] flex hover:bg-[#616161] hover:bg-opacity-50 rounded-xl transition duration-300 cursor-pointer"
+                  className="mr-10 py-6 bg-[#3B3B3B] flex hover:bg-[#616161] hover:bg-opacity-50 rounded-xl transition duration-300 cursor-pointer"
                   variant="primary"
                 >
                   <Image
@@ -75,13 +75,13 @@ export default function DashboardDetalhado() {
               </DropdownMenuContent>
             </DropdownMenu>
           </div>
-          <div className="lg:col-start-5 lg:col-span-2 md:col-span-2 md:col-start-5 col-span-4 col-start-2">
-            <Button className="w-full h-14 bg-[#3270B8] hover:bg-[#2D64A5] hover:bg-opacity-50 mt-10">
+          <div>
+            <Button className="w-80 h-14 bg-[#3270B8] hover:bg-[#2D64A5] hover:bg-opacity-50">
               Salvar alterações
             </Button>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-6 mx-10 my-5 gap-8">
+        <div className="flex mx-10 mt-5 gap-8">
           <CardUpdate></CardUpdate>
           <CardUpdate></CardUpdate>
         </div>
