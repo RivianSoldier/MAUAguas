a420fb8d00d25ddc9ecb4de73b8ee8e20295963e
Eduardo <eduardoc.santiago@hotmail.com>
2024-05-17 03:30:32 -0300

    otimização

--- front/mauaguas_front/components/ui/modelo-nivel-geral.jsx
+++ front/mauaguas_front/components/ui/modelo-nivel-geral.jsx
@@ -4,13 +4,17 @@ import {
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
-  } from "@/components/ui/tooltip"
+} from "@/components/ui/tooltip"
 
-export function PocoNivel({ altura, capacidade, nome }) {
+export function ModeloNivel({ altura, capacidade, nome, tipo }) {
+    const alturaPoco = "h-[515px]";
+    const alturaCaixa = "h-[250px]";
+    const tamanhoPoco = "h-[420px]";
+    const tamanhoCaixa = "h-[155px]";
 
     var cor = "#3B3B3B";
     var bgcor = "bg-[#3B3B3B]";
-    var textcor ="text-[#3B3B3B]"
+    var textcor = "text-[#3B3B3B]"
     var volume = (altura / capacidade) * 100
     var alert = ""
 
@@ -18,7 +22,7 @@ export function PocoNivel({ altura, capacidade, nome }) {
         cor = "#3B3B3B";
         bgcor = "bg-[#89CAF9]"
         textcor = "text-[#89CAF9]"
-        alert =""
+        alert = ""
     } else if (volume > 50) {
         cor = "#89CAF9"
         bgcor = "bg-[#89CAF9]"
@@ -37,7 +41,7 @@ export function PocoNivel({ altura, capacidade, nome }) {
     }
 
     return (
-        <div className="relative h-[515px] w-full min-w-[192px] bg-[#3B3B3B] rounded-lg row-span-2 col-span-1 drop-shadow-lg z-30">
+        <div className={`relative ${tipo == 1 ? alturaPoco : alturaCaixa} w-full min-w-[192px] bg-[#3B3B3B] rounded-lg ${tipo == 1 ? "row-span-2" : "row-span-1"} col-span-1 drop-shadow-lg z-30`}>
             <div className="flex justify-between items-center py-6 px-9">
                 <TooltipProvider>
                     <Tooltip>
@@ -55,7 +59,7 @@ export function PocoNivel({ altura, capacidade, nome }) {
                     <Tooltip>
                         <TooltipTrigger asChild>
                             <div className={`min-w-6 z-20 ${volume > 75 ? "hidden" : ""}`}>
-                                <svg                                        
+                                <svg
                                     width="25px"
                                     height="25px"
                                     viewBox="0 0 20 20"
@@ -92,10 +96,10 @@ export function PocoNivel({ altura, capacidade, nome }) {
                             <span className="text-base font-normal">{` / ${capacidade} m`}</span>
                         </p>
                     </div>
-                    <div className="absolute border-[3px] rounded-lg w-[196px] h-[40px] z-20" />
-                    <div className="absolute border-[3px] rounded-lg w-[196px] h-[56px] z-10" />
-                    <Progress height="h-[400px]" value={altura && capacidade ? (altura / capacidade) * 100 : 0} 
-                    color={bgcor} className="w-[196px] z-0" />
+                    <div className={`absolute border-[3px] rounded-lg w-[196px] h-[40px] ${tipo == 1 ? "" : "hidden"} z-20`} />
+                    <div className={`absolute border-[3px] rounded-lg w-[196px] ${tipo == 1 ? "h-[56px]" : "h-[30px]"} z-10`} />
+                    <Progress height={`${tipo == 1 ? tamanhoPoco : tamanhoCaixa}`} value={altura && capacidade ? (altura / capacidade) * 100 : 0}
+                        color={bgcor} className="w-[196px] z-0" />
                 </div>
             </div>
         </div>
