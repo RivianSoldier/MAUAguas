1a1fec179a12a6e719311e0b8f73f7777af1e9e1
Eduardo <eduardoc.santiago@hotmail.com>
2024-05-22 19:10:36 -0300

    overflow

--- front/mauaguas_front/app/dashboard-detalhado/page.js
+++ front/mauaguas_front/app/dashboard-detalhado/page.js
@@ -9,32 +9,112 @@ import { ModeloVazao } from "@/components/ui/modelo-vazao-geral";
 
 export default function DashboardDetalhado() {
   return (
+    <div className="h-[000px] bg-[#303030]">
     <main className="flex min-h-screen flex-col bg-[#303030]">
       <NavBar activePage="dashboard-detalhado" />
       <div className="flex flex-grow">
-        <div className="flex flex-col items-center justify-start pt-28 w-96 bg-[#3b3b3b] gap-4 px-4">
+        <div className="flex flex-col items-center justify-start pt-28 w-[22rem] bg-[#3b3b3b] gap-4 shadow-lg rounded-r-xl fixed">
           <div className="relative">
             <Input
               placeholder="Procurar..."
-              className="bg-[#505050] border-none text-white w-72 h-14"
+              className="bg-[#505050] border-none text-white w-[19rem] h-14"
             />
             <Search className="absolute right-2 top-5 h-5 w-5 text-white" />
           </div>
-
-          <div className="flex flex-row h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
-            <p className="pl-3">Poço</p>
-            <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
-              1500 m
+          <div className="flex flex-col gap-3 overflow-y-auto overflow-x-hidden pb-80 pr-2 h-screen">
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
             </div>
-          </div>
-          <div className="flex flex-row h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
-            <p className="pl-3">Poço</p>
-            <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
-              1500 m
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
+            </div>
+            <div className="flex flex-row min-h-14 w-72 border-[#505050] border-2 rounded-lg justify-between items-center text-white cursor-pointer hover:scale-[1.03] duration-75">
+              <p className="pl-3">Poço</p>
+              <div className="flex items-center justify-end pr-3 rounded-r-md bg-[#89caf9] h-full w-20">
+                1500 m
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col w-full text-white mx-4">
+        <div className="flex flex-col pb-4 w-full text-white mx-4 ml-[23rem]">
           <p className="mt-28">
             Dashboard em detalhes da
             <span className="font-bold"> Caixa D'água 4</span>
@@ -63,5 +143,6 @@ export default function DashboardDetalhado() {
         </div>
       </div>
     </main>
+    </div>
   );
 }
