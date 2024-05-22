1bb540af2ffb3ccebab79fc39bd324d78ce7b8c7
Eduardo <eduardoc.santiago@hotmail.com>
2024-05-22 04:40:42 -0300

    melhorias

--- front/mauaguas_front/components/ui/chart.jsx
+++ front/mauaguas_front/components/ui/chart.jsx
@@ -13,44 +13,44 @@ import {
 const data = [
   {
     name: "Page A",
-    uv: 4000,
-    pv: 2400,
+    Vazão: 4000,
+    Nível: 2400,
     amt: 2400,
   },
   {
     name: "Page B",
-    uv: 3000,
-    pv: 1398,
+    Vazão: 3000,
+    Nível: 1398,
     amt: 2210,
   },
   {
     name: "Page C",
-    uv: 2000,
-    pv: 9800,
+    Vazão: 2000,
+    Nível: 9800,
     amt: 2290,
   },
   {
     name: "Page D",
-    uv: 2780,
-    pv: 3908,
+    Vazão: 2780,
+    Nível: 3908,
     amt: 2000,
   },
   {
     name: "Page E",
-    uv: 1890,
-    pv: 4800,
+    Vazão: 1890,
+    Nível: 4800,
     amt: 2181,
   },
   {
     name: "Page F",
-    uv: 2390,
-    pv: 3800,
+    Vazão: 2390,
+    Nível: 3800,
     amt: 2500,
   },
   {
     name: "Page G",
-    uv: 3490,
-    pv: 4300,
+    Vazão: 3490,
+    Nível: 4300,
     amt: 2100,
   },
 ];
@@ -78,14 +78,14 @@ const CustomTooltip = ({ active, payload, label }) => {
             justifyContent: "space-between",
           }}
         >
-          <p style={{ fontWeight: 600, marginRight: "4px" }}>UV:</p>
-          <p style={{}}>{`${payload[0].value}`}</p>
+          <p style={{ fontWeight: 600, marginRight: "4px" }}>Vazão:</p>
+          <p style={{}}>{`${payload[0].value} L`}</p>
           <p
             style={{ fontWeight: 600, marginLeft: "20px", marginRight: "4px" }}
           >
-            PV:{" "}
+            Nível:{" "}
           </p>
-          <p>{`${payload[1].value}`}</p>
+          <p>{`${payload[1].value} L/s`}</p>
         </div>
       </div>
     );
@@ -113,21 +113,24 @@ function Chart() {
           bottom: 24,
         }}
       >
-        <CartesianGrid strokeDasharray="3" vertical={false} stroke="#606060" />
-        <XAxis dataKey="name" tick={{ fill: "white" }} />
-        <YAxis tick={{ fill: "white" }} />
+        <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#606060" />
+        <XAxis dataKey="name" tick={{ fill: "white" }} label={{ value: 'Tempo', position: 'top' }} />
+        <YAxis yAxisId="left" tick={{ fill: "white" }} label={{ value: 'NÍvel (L)', angle: -90, position: 'left' }} />
+        <YAxis yAxisId="right" orientation="right" tick={{ fill: "white" }}  label={{ value: 'Vazão (L/s)', angle: 90, position: 'right' }} />
         <Tooltip content={<CustomTooltip />} />
         <Legend />
         <Line
+          yAxisId={"left"}
           type="monotone"
-          dataKey="pv"
+          dataKey="Nível"
           stroke="#3270B8"
           activeDot={{ r: 8 }}
           strokeWidth={3}
         />
         <Line
+          yAxisId={"right"}
           type="monotone"
-          dataKey="uv"
+          dataKey="Vazão"
           stroke="#89caf9"
           strokeWidth={3}
           activeDot={{ r: 6 }}
