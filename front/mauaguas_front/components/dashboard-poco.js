438b5f1bcac0b0d0fca6caf84f2ca8e6b758568b
Eduardo <eduardoc.santiago@hotmail.com>
2024-05-28 19:37:24 -0300

    só teste

--- front/mauaguas_front/components/dashboard-poco.js
+++ front/mauaguas_front/components/dashboard-poco.js
@@ -13,7 +13,7 @@ export default function DashboardPoco() {
                 19:46 OS DADOS FORAM ATUALIZADOS !
             </div>
             <div className="grid grid-cols-4 gap-3 mt-3 grid-rows-3">
-                <div className="row-span-2 col-span-1">
+                <div className="row-span-2 col-span-4 min-[1190px]:col-span-1 min-[1190px]:row-span-2">
                     <ModeloNivel
                         altura={50}
                         capacidade={100}
@@ -21,22 +21,30 @@ export default function DashboardPoco() {
                         tipo={1}
                     />
                 </div>
-                <div className="row-span-2 col-span-3 h-[515px]">
+                <div className="row-span-2 col-span-4 h-[515px] min-[1190px]:col-span-3 min-[1190px]:row-span-2">
                     <Chart />
                 </div>
-                <div className="col-span-1">
+                <div className="col-span-2 min-[1190px]:col-span-1">
                     <ModeloVazao vazao={100} maximo={1000} hidden={true} />
                 </div>
-                <div className="col-span-1 bg-[#3B3B3B] rounded-lg drop-shadow-lg h-full flex flex-col items-center justify-evenly">
+                <div className="col-span-2 min-[1190px]:col-span-1 bg-[#3B3B3B] rounded-lg drop-shadow-lg h-full flex flex-col items-center justify-evenly row-span-">
                     <p className="text-center font-bold w-[80%]">Tempo de operação da bomba</p>
-                    <div className="h-[150px] w-[150px] rounded-full border-[15px] border-white flex flex-col justify-end items-center">
+                    <div className="h-[150px] w-[150px] rounded-full border-[15px] border-white flex flex-col justify-center items-center">
                         <p className="font-bold text-2xl">12 h</p>
-                        <p className="bg-[#3B3B3B] mt-4 -mb-4 p-3">Entrada</p>
                     </div>
                 </div>
-                <div className="col-span-2 bg-[#3B3B3B] rounded-lg drop-shadow-lg h-full">
+                <div className="col-span-4 bg-[#3B3B3B] rounded-lg drop-shadow-lg h-full flex flex-col justify-between min-[1190px]:col-span-2">
                     <p className="font-bold pt-6 pl-8">Pressão de entrada e saída do filtro</p>
-                    
+                    <div className="flex justify-evenly items-center mb-3">
+                        <div className="h-[150px] w-[150px] rounded-full border-[15px] border-[#89CAF9] flex flex-col justify-end items-center">
+                            <p className="font-bold text-2xl">130 PSI</p>
+                            <p className="bg-[#3B3B3B] mt-4 -mb-4 p-3 text-[#89CAF9]">Entrada</p>
+                        </div>
+                        <div className="h-[150px] w-[150px] rounded-full border-[15px] border-[#F989B2] flex flex-col justify-end items-center">
+                            <p className="font-bold text-2xl">82 PSI</p>
+                            <p className="bg-[#3B3B3B] mt-4 -mb-4 p-3 text-[#F989B2]">Saída</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
