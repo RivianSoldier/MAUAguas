dd83e6427e5e4132a36ad755641a7130755bf9b1
fernandokusko <fernando.freitas@bcrcx.com>
2024-06-09 21:11:14 -0300

    EU odeio esse PI

--- back/api/alerts.py
+++ back/api/alerts.py
@@ -28,8 +28,8 @@ class Alerts:
         
     @staticmethod
     def alert3(id, alert_value):
-        account_sid = 'AC029b4069c50a254f056ee17016a92c82'
-        auth_token = 'fc6603f7ccfce40849677a6bd7990c70'
+        account_sid = ''
+        auth_token = ''
         client = Client(account_sid, auth_token)
         message = client.messages.create(
         from_='whatsapp:+14155238886',
