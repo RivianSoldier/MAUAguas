635cf2acf300490a54634f3808a21dcf2d147eeb
fernandokusko <fernando.freitas@bcrcx.com>
2024-06-09 21:11:44 -0300

    EU odeio esse PI

--- back/api/twilio.py
+++ back/api/twilio.py
@@ -3,8 +3,8 @@ from twilio.rest import Client
 
 def twilio(id, alert_value):
 
-    account_sid = 'AC029b4069c50a254f056ee17016a92c82'
-    auth_token = 'fc6603f7ccfce40849677a6bd7990c70'
+    account_sid = ''
+    auth_token = ''
     client = Client(account_sid, auth_token)
     message = client.messages.create(
     from_='whatsapp:+14155238886',
